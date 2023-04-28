f8e55c72134a0ac64f1e01934e118e3c1db5a840
m-destiny <mehulr2801@gmail.com>
2023-04-29 00:50:06 +0530

    zala ata no more changes hope so

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://zenithia.onrender.com/api/v1/dalle', {  
+      const response = await fetch('https://zen-b5q4.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
